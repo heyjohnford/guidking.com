f7c7a24fa80c6f4fe5c19eabdc81a8afff16f8b9
John Ford <johnamiahford@gmail.com>
2019-01-20 01:10:59 -0500

    Reorganize structure of file tree Add tailwind.css to package.json Add manifest assets

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { ReactComponent as Logo } from './logo.svg';
-import './App.css';
+import { ReactComponent as Logo } from '../assets/logo.svg';
+import '../css/App.css';
 
 class App extends Component {
   render() {
@@ -8,15 +8,16 @@ class App extends Component {
       <div className="app-container pt-32">
         <div className="app-item app-item1 mr-12">
           <header>
-            <a className="inline-flex mb-5" href="/">
+            <a className="app-logo inline-flex mb-5" href="/">
+              Home
               <Logo />
             </a>
           </header>
           <hr className="app-hr" />
-          <h1 className="app-title app-font font-semibold leading-tight mt-12 mb-10">Generate Some beautiful<br />GUIDs for your project</h1>
+          <h1 className="app-title app-font font-semibold leading-tight mt-12 mb-10">Generate some beautiful<br />GUIDs for your project</h1>
           <p className="app-text-secondary leading-normal w-5/6">Use GUIDs as key identifiers for your event-driven architecture or to keep track of your system’s inventory. GUIDs are great, we’re here to help you generate them.</p>
           <div className="app-guidcounter app-font font-semibold italic mt-32">
-            <p className="text-blue-darkest">So Far, <span className="text-blue font-bold text-3xl">2,203</span> GUIDs Created</p>
+            <p className="text-grey-darker">So Far, <span className="text-blue font-bold text-3xl">2,203</span> GUIDs Created</p>
           </div>
         </div>
         <div className="app-item app-item2">
@@ -28,7 +29,7 @@ class App extends Component {
                   <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="amount">
                     Amount
                   </label>
-                  <input className="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darkest leading-loose focus:outline-none focus:bg-white focus:border-blue" id="number-of-guids" name="number-of-guids" type="text" placeholder="Number of GUIDs..." />
+                  <input className="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darkest leading-loose focus:outline-none focus:bg-white focus:border-blue" id="amount" name="amount" type="text" placeholder="Number of GUIDs..." />
                 </div>
                 <small className="text-grey-dark"><em>**No more than 5,000 GUIDs please</em></small>
                 <hr className="app-hr mt-4" />
@@ -49,7 +50,11 @@ class App extends Component {
                 <hr className="app-hr" />
               </div>
               <div className="flex items-center justify-between">
-                <button className="app-btn bg-blue text-white text-xl font-bold w-full py-6 rounded rounded-t-none hover:bg-blue-dark focus:outline-none focus:shadow-outline" type="button">
+                <button
+                  className="app-btn bg-blue text-white text-xl font-bold w-full py-6 rounded rounded-t-none hover:bg-blue-dark focus:outline-none focus:shadow-outline"
+                  type="button"
+                  aria-label="Generate"
+                >
                   Generate GUIDs
                 </button>
               </div>
