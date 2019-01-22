12dac51c15a69dc0e720c01677654cebf2b496f3
John Ford <johnamiahford@gmail.com>
2019-01-22 08:54:31 -0500

    Stateful logic move to form component Add guid formatter for different options

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,81 @@
 import React, { useState } from 'react'
 import FormItem from './FormItem'
 import { formOptions } from '../constants'
+import { BASE_API } from '../constants'
+import helpers from '../helpers'
 
 const Form = (props) => {
-  const checkmarkOptions = formOptions.map(o => <FormItem key={o.name} name={o.name} checked={o.checked} />)
-  const [amount, setAmount] = useState('1')
+  const [state, setState] = useState({
+    guids: [],
+    guidAmount: 1,
+    formOptions: formOptions || []
+  })
+  const checkmarkOptions = state.formOptions.map(o => (
+    <FormItem
+      key={o.name}
+      name={o.name}
+      checked={o.checked}
+      handleIsCheckedChange={handleIsCheckedChange}
+    />
+  ))
+
+  function handleIsCheckedChange(e) {
+    const { name, checked } = e.target
+    const options = state.formOptions.map(o => o.name === name ? { ...o, checked } : o)
+
+    setState({ ...state, formOptions: options })
+  }
+
+  async function handleGenerateGuidsSubmit(e) {
+    e.preventDefault()
+
+    try {
+      const response = await (await fetch(`${BASE_API}?amount=${state.guidAmount}`)).json()
+
+      if (!response.error) {
+        setState({ ...state, guids: response })
+        return
+      }
+
+      reportError(response)
+      console.log(response)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  function reportError(err) {
+    alert(err.message)
+  }
 
   function handleAmountChange(e) {
-    setAmount(e.target.value)
+    const value = e.target.value
+
+    if (!/^(\s*|\d+)$/.test(value) || value.length > 4) {
+      return
+    }
+
+    setState({ ...state, guidAmount: value  })
+  }
+
+  function formatGuids(guids = []) {
+    return state.formOptions.reduce((modifiedGuids, option) => {
+      const funcName = option.name.toLowerCase() + 'Pipeline'
+
+      if (option.checked && option.name !== formOptions[0].name) {
+        modifiedGuids = helpers[funcName](modifiedGuids)
+      }
+
+      if (!option.checked && option.name === formOptions[0].name) {
+        modifiedGuids = helpers[funcName](modifiedGuids)
+      }
+
+      return modifiedGuids
+    }, guids)
   }
 
   return (
-    <form className="app-form bg-white shadow-md rounded">
+    <form className="app-form bg-white shadow-md rounded" onSubmit={handleGenerateGuidsSubmit}>
       <div className="app-form-container px-8 pt-6 pb-8 mb-3">
         <h2 className="app-title-secondary text-lg mb-6">Go Ahead, Get Your GUID On</h2>
         <div className="mb-3">
@@ -23,7 +87,7 @@ const Form = (props) => {
             id="amount"
             name="amount"
             type="text"
-            value={amount}
+            value={state.guidAmount}
             onChange={handleAmountChange}
             placeholder="Number of GUIDs..."
           />
@@ -35,12 +99,14 @@ const Form = (props) => {
         </div>
         <hr className="app-hr" />
       </div>
+      <div>{state.guids}</div>
+      <div>{formatGuids(state.guids)}</div>
       <div className="flex items-center justify-between">
         <button
           className="app-btn bg-blue text-white text-xl font-bold w-full py-6 rounded rounded-t-none hover:bg-blue-dark focus:outline-none focus:shadow-outline"
           type="button"
           aria-label="Generate"
-          onClick={props.handleGenerateGuidsClick}
+          onClick={handleGenerateGuidsSubmit}
         >
           Generate GUIDs
         </button>
