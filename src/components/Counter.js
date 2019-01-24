e006040bd8bcc613860bcd2a91a870021e71f308
John Ford <johnamiahford@gmail.com>
2019-01-23 23:22:37 -0500

    Add animation logic using Spring

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Spring, config } from'react-spring'
 
 export default (props) => {
-    return (
-      <div className="app-guidcounter app-font font-semibold italic mt-32">
-        <p className="text-grey-darker">So Far, <span className="text-blue font-bold text-3xl">2,203</span> GUIDs Created</p>
-      </div>
-    )
+  return (
+    <div className="app-guidcounter app-font font-semibold italic mt-32">
+      <p className="text-grey-darker">So Far,
+        <Spring
+          from={{ number: 0 }}
+          to={{ number: props.totalGuids }}
+          delay="200"
+          config = { config.default }
+        >
+          {props => <span className="text-blue font-bold text-3xl"> {Number(props.number.toFixed()).toLocaleString()} </span>}
+        </Spring>
+        GUIDs Created
+      </p>
+    </div>
+  )
 }
