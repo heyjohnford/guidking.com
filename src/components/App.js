29c890d5cf9d31b029579eba5f095960d80013b7
John Ford <johnamiahford@gmail.com>
2019-01-23 23:22:06 -0500

    Add socket.io client

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,33 @@ import Header from './Header'
 import Counter from './Counter'
 import Form from './Form'
 import Footer from './Footer'
+import { gofetch } from '../helpers'
+import { SOCKET_API } from '../constants'
+import socket from 'socket.io-client'
 import '../css/App.css'
 
+const  clientSocket = socket(SOCKET_API + 'total')
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { totalGuids: 0 }
+  }
+
+  async initialGuidTotal() {
+    const response = await gofetch('total')
+    this.setState({ totalGuids: response.total })
+  }
+
+  subscribeToCounter() {
+    clientSocket.on('counter', (data) => this.setState({ totalGuids: data.total }))
+  }
+
+  async componentDidMount() {
+    await this.initialGuidTotal()
+    this.subscribeToCounter()
+  }
+
   render() {
     return (
       <section className="app-container pt-32">
@@ -14,7 +38,7 @@ class App extends Component {
           <hr className="app-hr" />
           <h1 className="app-title app-font font-semibold leading-tight mt-12 mb-10">Generate some beautiful<br />GUIDs for your project</h1>
           <p className="app-text-secondary leading-normal w-5/6">Use GUIDs as key identifiers for your event-driven architecture or to keep track of your system’s inventory. GUIDs are great, we’re here to help you generate them.</p>
-          <Counter />
+          <Counter totalGuids={this.state.totalGuids} />
         </div>
         <div className="app-item app-item2">
           <div className="w-full max-w-sm">
